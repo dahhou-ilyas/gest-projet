a1eab9f8524550aab98c1a0e6304fe96d2a06edd
dahhou ilyas <dahhou.ilyas@gmail.com>
2024-02-23 01:26:44 +0100

    test

--- app/index.js
+++ app/index.js
@@ -3,13 +3,16 @@ const app=express();
 const bodyParser=require('body-parser');
 const dotenv = require('dotenv');
 const cookieParser = require('cookie-parser');
+const cors = require('cors');
 dotenv.config();
 
 const userRoute=require('./routes/userRoute')
 const taskRoute=require('./routes/taskRoute')
 
+app.use(cors());
 app.use(cookieParser());
 app.use(bodyParser.json());
+
 
 app.use('/users',userRoute);
 app.use('/tasks',taskRoute);
