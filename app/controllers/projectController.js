d141897287029a30f072adb10d6a86947a6bc7d0
dahhou ilyas <dahhou.ilyas@gmail.com>
2024-03-07 14:55:57 +0100

    add project controller

---
+++ app/controllers/projectController.js
@@ -0,0 +1,27 @@
+const Project = require("../model/Project");
+const User = require("../model/User");
+
+module.exports={
+    createProject:async (req,res)=>{
+        const userId=req.params.userId;
+        const {title,descriptionProjet}=req.body;
+        try {
+            const user=await User.findById(userId);
+            if(!user){
+                res.status(404).json({ message: 'Utilisateur non trouvé' });
+            }
+
+            const newProject=new Project({title,descriptionProjet})
+            await newProject.save();
+
+            user.projects.push(newProject._id);
+
+            await user.save();
+
+            res.status(201).json(newProject);
+        } catch (error) {
+            console.error('Erreur lors de l\'ajout du projet à l\'utilisateur :', error);
+            res.status(500).json({ error: 'Erreur serveur lors de l\'ajout du projet à l\'utilisateur' });
+        }
+    }
+}
