ec0cd5728a06a3ab64242d4889e6fe68f057a938
dahhou ilyas <dahhou.ilyas@gmail.com>
2024-03-26 21:47:23 +0000

    add frontend projetc

---
+++ server/app/model/Group.js
@@ -0,0 +1,42 @@
+const mongoose=require('mongoose');
+
+const GroupSchema=new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    users: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    }],
+    createdBy: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    invitationToken: {
+        type: String,
+    },
+    projects: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Project'
+    }]
+});
+
+GroupSchema.pre('save',async function (next){
+    if(!this.isNew){
+        return next();
+    }
+    try {
+        if(!this.users.includes(this.cretedBy)){
+            this.users.push(this.cretedBy)
+        }
+        next();
+    } catch (error) {
+        next(error)
+    }
+})
+
+const Group=mongoose.model('Group',GroupSchema);
+
+module.exports = Group
