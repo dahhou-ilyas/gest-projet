51853141062799c4747e80520e06da77dbe8eff4
dahhou ilyas <110790236+dahhou-ilyas@users.noreply.github.com>
2024-03-31 17:44:02 +0000

    Update dev.test.js

--- server/tests/dev.test.js
+++ server/tests/dev.test.js
@@ -12,7 +12,7 @@ let globalToken;
 let groupCreted;
 
 beforeAll(async () => {
-    await mongoose.connect("mongodb://127.0.0.1:27017/gest-projet");
+    await mongoose.connect("mongodb://localhost:27017/gest-projet");
 });
 
 afterAll(async () => {
